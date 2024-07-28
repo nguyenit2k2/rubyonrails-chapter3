13f516d6cefb723925c65ccd9fec22696d0219fc
Hoang Nguyen <nntn.1911o.o@gmail.com>
2024-07-29 02:35:14 +0700

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,10 @@ class ActiveSupport::TestCase
 
   # Set up all fixtures in test/fixtures/*.yml.
   fixtures :all
-  include ApplicationHelper
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 
   # Add more helper methods to be used by all tests here...
 end
